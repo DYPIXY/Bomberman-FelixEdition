2e81041b9fd19c4dff43cc25e0311f29d50628fb
AzaltoKing <Azaltokingoficial@gmail.com>
2026-05-25 00:04:32 -0300

    mudei um comentario

--- inimigo.h
+++ inimigo.h
@@ -60,8 +60,7 @@ void criarInimigo(int mapa[hMax][wMax]) {
             if (mapa[i][j] == 99) {
                 criarInimigo(j, i); // Chama a função original (x=j, y=i)
                 
-                // Opcional: Limpa o '99' do mapa e coloca chão (0) 
-                // para o inimigo não nascer "dentro" de um bloco sólido
+                //  Limpa o '99' do mapa e coloca chão (0) 
                 mapa[i][j] = 0; 
             }
         }
